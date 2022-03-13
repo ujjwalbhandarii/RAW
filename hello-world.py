8ba4da3370e25d3e2c3a396e8717283f4bb91aa5
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-03-13 14:53:32 +0545

    print - escape sequence - comments

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,16 @@
 # this is my 1st code in python
 print('hello world')
+
+# another
+print("demo line to show how to not print in new line ",
+      end="")   # end=""  is used  -- also called as new line character
+print("hello world")
+
+
+# escape sequence
+print("ujjwal is a good by \t he is shy. Even though \nhe is the most handsome boy in the class")
+
+
+# Comments
+# 1. using hash ( # )
+# 2. using triple double quote ( """"  """")
