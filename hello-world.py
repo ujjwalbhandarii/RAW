8a54ffe896f8cf0629ba230125bbe99ac8f31d82
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-03-13 23:33:11 +0545

    print - comments - escape sequence

--- hello-world.py
+++ hello-world.py
@@ -1,13 +1,27 @@
 # this is my 1st code in python
 print('hello world')
 
-# another
+# other
 print("demo line to show how to not print in new line ",
       end="")   # end=""  is used  -- also called as new line character
 print("hello world")
 
 
-# escape sequence
+# escape sequence in python
+""""
+These are some common escape sequence in python
+\'	Single Quote	
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed	
+\ooo	Octal value	
+\xhh	Hex value	
+
+"""
+
 print("ujjwal is a good by \t he is shy. Even though \nhe is the most handsome boy in the class")
 
 
