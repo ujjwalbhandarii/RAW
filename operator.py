1166d234c68882ca17fca95af6df5c88285f2d20
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-03-14 22:26:48 +0545

    opearators

---
+++ operator.py
@@ -0,0 +1,16 @@
+
+# Arthemetic operator
+print("15 // 17 :", 15 // 17)   # // gives the output as integer 
+print("15 / 17 :", 15 / 17)     # gives the actual floating number
+print("15 ** 17 :", 15 ** 17)     # exponential operator ---> 15 powers 17
+print("15 % 3 :", 15 % 3)     # gives remainder
+
+# Assignment opeartor
+# Comparision operator
+# logical opeartor  (and or )
+
+# identity opeartors (is, is not)
+
+# membership opeartor ( in, not in)
+
+# Bitwise opeartor -- works with binary numbers
