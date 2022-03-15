383a88849268d69406bc8adc14c450529cdfc69c
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-03-15 12:17:19 +0545

    test

--- aaaaTest.py
+++ aaaaTest.py
@@ -2,25 +2,25 @@
 # Take input from user and give feedback about
 # does the number is greater or smaller than the actual number you printed
 
-n = 18  # our actual number
+# n = 18  # our actual number
 
-num = int(input("Enter a number: "))
-noOfGuesses = 0
-print("You have Total 3 guesses")
+# num = int(input("Enter a number: "))
+# noOfGuesses = 0
+# print("You have Total 3 guesses")
 
-while (noOfGuesses < 10):
-    if num == n:
-        print("you are correct")
-        break
-    elif num > n:
-        print("your number is greater")
-        noOfGuesses += 1
-        continue
-    elif num < n:
-        print("your number is smaller")
-        noOfGuesses += 1
-        continue
-    else:
-        print("invalid input")
+# while (noOfGuesses < 10):
+#     if num == n:
+#         print("you are correct")
+#         break
+#     elif num > n:
+#         print("your number is greater")
+#         noOfGuesses += 1
+#         continue
+#     elif num < n:
+#         print("your number is smaller")
+#         noOfGuesses += 1
+#         continue
+#     else:
+#         print("invalid input")
 
-print("Game over")
+# print("Game over")
