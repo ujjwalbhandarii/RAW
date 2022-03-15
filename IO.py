6ab588bfa99797438f784dcaf4332c46888d3688
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-03-15 16:43:23 +0545

    file - handeling

--- IO.py
+++ IO.py
@@ -43,3 +43,47 @@ print(c.readline())  # only prints one line
 
 e = open("sensativeInfo.txt", "r")
 print(e.readlines())   # this will print lines in the form of list
+
+
+
+
+
+
+# writing in a file 
+f = open("ujjwal.txt","w")   
+f.write("ujjwal is a cool boy")
+
+f.close()
+
+
+# writing in appending mode
+f = open("ujjwal.txt","a")  # this will add the .write("content") in the file except erasing all
+f.write("ujjwal is a cool boy")
+
+f.close()
+
+# characters written in a file
+f = open("ujjwal.txt","a")
+abc = f.write("this content will be added")
+
+print(a)
+f.close()
+
+
+# from user input
+f = open("ujjwal.txt","a")
+content = input("enter the text to write in the file\n")   # taking user input to write in a file
+abc = f.write(content)   
+
+print(abc)    # this will read the character length
+f.close()
+
+
+# handel read and write both   -- r+
+f = open("sensativeInfo.txt","r+")
+
+print(f.read()) # this will read from file and print in terminal
+f.write("\nthis content will be added")   # this will write agian
+
+
+f.close()
