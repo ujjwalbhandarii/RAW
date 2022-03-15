58b2f74f28c761869f28dcb7a8b6fba27e6a2131
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-03-15 13:09:01 +0545

    exceptation handeling

--- try_except.py
+++ try_except.py
@@ -6,7 +6,7 @@ b = input("enter another number: ")
 try:
     print("sum of two number is",int(a)+int(b))
 
-except Exception as e:
+except Exception as e:    # as is a keyword
     print(e)
 
 print("this line will be executed now")
