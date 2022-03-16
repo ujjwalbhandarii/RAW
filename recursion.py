87343848bb7f9bdf65c8d790c7867405e091f167
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-03-16 15:33:58 +0545

    recursion

---
+++ recursion.py
@@ -0,0 +1,30 @@
+# recursive -- using function inside function
+
+
+
+
+# iterative approch to find factoial
+def iFacto(n):
+    
+    fac = 1
+    for i in range(n):
+        fac = fac * (i+1)
+    return fac
+
+number = int(input("enter a number: "))
+print(iFacto(number))
+
+
+
+
+
+# recursive methood
+def rFacto(n):
+
+    if n == 1:
+        return 1
+    else:
+        return n * rFacto(n-1)
+
+num = int(input("enter a number for recursive: "))
+print(rFacto(num))
