2f111a64d532f7f81e34a67396ab4e2c50e15017
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-03-16 17:12:56 +0545

    pattern solving

--- exercise/PatternProblem.py
+++ exercise/PatternProblem.py
@@ -2,27 +2,24 @@
 
 
 num = int(input("Enter the number: "))
-decide = int(input("1. True\n2. False\n"))
+print("PRESS")
+decide = int(input("1. Upper Triangle\n0. Lower Triangle\n"))
 
-if decide == 1:
-    decide = "True"
-    
-elif decide == 2:
-    decide = "False"
+new = bool(decide)
 
-if decide == "True":
-    print("you entered 1")
+if new == True:
+    # print("you entered 1")
     for i in range(num):
         for j in range(i+1):
             print("# ", end="")
-    print()
+        print()
                     
-elif decide == "False":
-    print("you are in 2")
+elif new == False:
+    # print("you are in 2")
     for i in range(num):
         for j in range(num-i):
             print("# ", end="")
-    print()
+        print()
 
 else:
         print("invalid!!")
