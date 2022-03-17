83aa001a554544656d806e356c27c336cc8b5737
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-03-17 14:27:50 +0545

    modules on python

---
+++ moduels.py
@@ -0,0 +1,25 @@
+
+import random
+  
+num = random.randint(0,5)
+print(num)
+
+
+rand = random.random()  #generating random form o - 1   so, answer must be in points
+print(rand)
+
+rand1 = random.random() * 100     # for generating 0 - 100 randoms
+print(rand1)
+
+
+# random and list 
+list = ["ujjwal", "ankit","prabesh","sid"]
+c = random.choice(list)
+print(c)
+
+
+
+
+
+
+# external and build in modules
