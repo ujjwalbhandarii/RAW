c0ccd0b39bc81df0f63a3b15889be1e11ae08227
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-03-17 20:20:30 +0545

    understanding imports

---
+++ file1.py
@@ -0,0 +1,6 @@
+import file2
+
+print(f"{file2.a} ujjwal")
+print(file2.b)
+
+file2.name("ujjwal")
---
+++ file2.py
@@ -0,0 +1,8 @@
+# this file is for import variables
+
+a = "how are you"
+b = 14
+
+
+def name(str):
+    print(f"This is file and im extracting file 2. it is written {str}")
