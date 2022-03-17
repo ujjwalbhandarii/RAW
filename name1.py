52a90f29b4ac33efda52957eebbaee0d9843c082
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-03-17 22:25:56 +0545

    if __name__ == 'main':

---
+++ name1.py
@@ -0,0 +1,15 @@
+
+def string1(str):
+    return(f"hello this is {str}")
+
+def add(a,b):
+    return a+b
+
+
+
+if __name__ == "__main__":   # this come handy while importing this file to other file. code below will not be exectued
+
+    print(string1("ujjwal"))
+
+    c = add(1,2)
+    print(c)
---
+++ name2.py
@@ -0,0 +1,5 @@
+from pip import main
+import name1
+
+print(name1.add(2,3)) # with the help of if __name__ == "__main__"
+
