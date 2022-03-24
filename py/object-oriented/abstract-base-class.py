631a9bf6f6ffed078fef137faa6cbe91fe821d2a
ujjwalbhandari14 <ujjwalbhandari14@gmail.com>
2022-03-24 16:38:37 +0545

    abstract base class -- abstract methods

---
+++ py/object-oriented/abstract-base-class.py
@@ -0,0 +1,27 @@
+
+# abc --> module
+# abstractmethod --> decorator
+from abc import ABCMeta, abstractmethod
+
+
+class Shape(metaclass=ABCMeta):  # can also write " ABC "
+    @abstractmethod
+    def printarea(self):
+        return 0
+
+
+class Rectange(Shape):
+    type = "Reactange"
+    sides = 4
+
+    def __init__(self) -> None:
+        self.length = 2
+        self.breadth = 2
+
+    def printarea(self):
+        return self.length * self.breadth
+
+
+ujjwal = Rectange()
+
+print(ujjwal.printarea())
