ee8d36352bbaf79019e82078e0267c4d599afe8b
ujjwalbhandari14 <ujjwalbhandari14@gmail.com>
2022-03-25 18:40:17 +0545

    libary management system

--- py/exercise/libary-mngt-sys.py
+++ py/exercise/libary-mngt-sys.py
@@ -1,7 +1,14 @@
 # create a libary class
 # display book
-# lend book
+# lend book  -->if lend not available: who owns the book
 # add book
 # return book
+
+# implement -> constructor takes "listofbooks" and "libaryname"
+
+
+# can do by self
+# maintain dictinary (key- books, value- name of person)
+
 
 # to do
