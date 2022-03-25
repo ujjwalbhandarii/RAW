74765e332c0dde077b98af297ddb8a546eaee9cc
ujjwalbhandari14 <ujjwalbhandari14@gmail.com>
2022-03-25 19:19:29 +0545

    generators

--- py/generators.py
+++ py/generators.py
@@ -72,3 +72,49 @@ x = simpleGeneratorFun()
 print(x.next())  # In Python 3, __next__()
 print(x.next())
 print(x.next())
+print()
+
+# As another example, below is a generator for Fibonacci Numbers.
+# A simple generator for Fibonacci Numbers
+
+
+def fib(limit):
+
+    # Initialize first two Fibonacci Numbers
+    a, b = 0, 1
+
+    # One by one yield next Fibonacci Number
+    while a < limit:
+        yield a
+        a, b = b, a + b
+
+
+# Create a generator object
+x = fib(5)
+
+# Iterating over the generator object using next
+print(x.next())  # In Python 3, __next__()
+print(x.next())
+print(x.next())
+print(x.next())
+print(x.next())
+
+# Iterating over the generator object using for
+# in loop.
+print("\nUsing for in loop")
+for i in fib(5):
+    print(i)
+
+
+"""
+Applications : 
+Suppose we to create a stream of Fibonacci numbers, 
+adopting the generator approach makes it trivial; we just have to call next(x) to get the next Fibonacci number 
+without bothering about where or when the stream of numbers ends.
+A more practical type of stream processing is handling large data files such as log files. 
+Generators provide a space efficient method for such data processing as only parts of the file are handled 
+at one given point in time. We can also use Iterators for these purposes, but Generator provides a quick way 
+(We don’t need to write __next__ and __iter__ methods here).
+
+
+"""
