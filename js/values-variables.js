9822fd6b38a72c4588ed401dfa4d189e3b0720a4
ujjwalbhandari14 <ujjwalbhandari14@gmail.com>
2022-03-30 19:18:22 +0545

    js

---
+++ js/values-variables.js
@@ -0,0 +1,9 @@
+console.log("hy from vs terminal using node");
+
+// camel casing is mostly perfered
+
+// 1st character must be letter, underscore(_) or $
+// vaiable name are case sensative
+var myName = "ujjwal";
+// (variable) (name) = (value)
+console.log(myName);
