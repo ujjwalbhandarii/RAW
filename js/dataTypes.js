5aa7bec28b49dbc8dd0450482674ea9ceb3e808f
ujjwalbhandari14 <ujjwalbhandari14@gmail.com>
2022-03-30 20:58:23 +0545

    data types

---
+++ js/dataTypes.js
@@ -0,0 +1,19 @@
+// there are 6 primitive dataType in js
+undefined;
+Boolean;
+Number;
+String;
+BigInt; // 2020 or 2019 ko echma script ma aayako theo
+Symbol; // echma script 2015
+
+let myName = "ujjwal";
+console.log(typeof myName);
+
+myName = 15;
+console.log(typeof myName);
+
+myName = 15.6;
+console.log(typeof myName);
+
+myName = true;
+console.log(typeof myName);
